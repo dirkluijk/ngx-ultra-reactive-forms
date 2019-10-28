524ca02a1cc3262ecb3554e3203337d207ef8931
Dirk Luijk <mail@dirkluijk.nl>
2019-10-28 12:58:10 +0100

    chore: add demo project

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { FormControl } from 'ngx-ultra-reactive-forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./app.component.scss'
+  ]
 })
-export class AppComponent {}
+export class AppComponent {
+  public formControl1 = new FormControl('foo');
+
+  public formControl2 = new FormControl(this.formControl1.value$, {
+      disabled$: this.formControl1.value$.pipe(map((value) => value!.length > 5))
+    }
+  );
+}
--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { UltraReactiveFormsModule } from 'ngx-ultra-reactive-forms';
 
 import { AppComponent } from './app.component';
 
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserAnimationsModule,
+    UltraReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
